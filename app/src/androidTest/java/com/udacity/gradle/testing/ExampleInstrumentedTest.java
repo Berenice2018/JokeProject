4f1ca9e234ebe89ac712c36ec307c1a02c0afdd8
Berenice <visualfan2012@gmail.com>
2018-06-05 21:51:32 +0200

    error connectedcheck

---
+++ app/src/androidTest/java/com/udacity/gradle/testing/ExampleInstrumentedTest.java
@@ -0,0 +1,48 @@
+package com.udacity.gradle.testing;
+
+/*
+ * Created by Berenice on 04.06.18.
+ */
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.AndroidTestCase;
+
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+
+
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+
+    @Test
+    public void testVerifyJoke() throws InterruptedException {
+        assertTrue(true);
+        final CountDownLatch latch = new CountDownLatch(1);
+        Context context = InstrumentationRegistry.getContext();
+        EndpointsAsyncTask testTask = new EndpointsAsyncTask() {
+            @Override
+            protected void onPostExecute(String result) {
+                assertNotNull(result);
+                if (result != null){
+                    assertTrue(result.length() > 0);
+                    latch.countDown();
+                }
+            }
+        };
+        testTask.execute(context);
+        latch.await();
+    }
+}
